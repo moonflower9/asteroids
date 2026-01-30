1a4b87720d29b6de14a12560e7b6e7db79115309
moon <csteel217@gmail.com>
2026-01-30 16:24:22 +1300

    add turning function

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 
@@ -22,3 +22,14 @@ class Player(CircleShape):
     def draw(self, screen):
         points = self.triangle()
         pygame.draw.polygon(screen, "white", points, LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation = PLAYER_TURN_SPEED * dt + self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
